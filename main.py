a6afe79ecc0082cf28b6d405dad15b126e04a4dd
MisterPasha <pavel.jermolajev99@gmail.com>
2024-11-12 19:43:19 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("350x600")
+
+w= root.winfo_width()
+h = root.winfo_height()
+
+button = ctk.CTkButton(root, text="Connect to Sensors").pack(pady=20)
+
+root.mainloop()
