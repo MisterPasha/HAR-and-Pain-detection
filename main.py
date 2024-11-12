431b00fd12e8b26ce391b81b81c4908bfa20d215
MisterPasha <pavel.jermolajev99@gmail.com>
2024-11-12 20:17:55 +0000

    test push

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ ctk.set_default_color_theme("green")
 root = ctk.CTk()
 root.geometry("350x600")
 
-w= root.winfo_width()
+# some comment
+w = root.winfo_width()
 h = root.winfo_height()
 
 button = ctk.CTkButton(root, text="Connect to Sensors").pack(pady=20)
